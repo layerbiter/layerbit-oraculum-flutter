cdf70469e112e529fd9092628f76785dc5a3e01b
sazonovOleg <o.sazonov@propellerads.net>
2025-11-05 20:43:03 +0300

    initial app

---
+++ android/app/src/main/kotlin/com/sports/oraculum/app/layerbit_oraculum_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sports.oraculum.app.layerbit_oraculum_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/application/application.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatefulWidget {
+  const Application({super.key});
+
+  @override
+  State<Application> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Application> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
